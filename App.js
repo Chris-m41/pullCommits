54506b9b92573d250bc1b29c43216482cde72d11
Christian Martinez <mar13137@byui.edu>
2022-01-31 16:16:39 -0600

    Made commit app

---
+++ App.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { Octokit } from 'octokit';
+import key from '../githubKey.txt';
+
+export const RetrieveCommits = () => {
+  const [commits, setCommits] = useState([]);
+  const octokit = new Octokit({ auth: key }); //NEED TO HIDE THIS
+
+  const getCommits = useCallback(async () => {
+    const owner = 'facebook';
+    const repo = 'react-native';
+    const per_page = 5;
+
+    const getCommits = await octokit.request(
+        `GET /repos/{owner}/{repo}/commits`, { owner, repo, per_page }
+      );
+
+    setCommits(getCommits);
+  }, []);
+
+  useEffect(() => {
+    getCommits()
+      .catch(console.error);
+  }, [getCommits]);
+
+  const renderItem = (item) => {
+    return (
+      <View style={{
+        backgroundColor: item.index % 2 === 0 ? '#ccc' : '#fff',
+        borderWidth: 1,
+        flexDirection: 'column',
+      }}>
+        <Text style={styles.item}>Username: {item.item.author.login}</Text>
+        <Text numberOfLines={1} style={styles.item}>Hash: {item.item.sha}</Text>
+        <Text numberOfLines={1} style={styles.item}>Message: {item.item.commit.message}</Text>
+      </View>
+    )
+  }
+
+  return (
+    <FlatList
+        data={commits.data}
+        renderItem={renderItem}
+      />
+  );
+};
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}>25 Latest pulls for React Native</Text>
+      <RetrieveCommits />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    padding: 2,
+    color: '#373f51',
+  },
+});
+
+export default App;
